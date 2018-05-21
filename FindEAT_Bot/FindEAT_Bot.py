6346ca98d01c64435a1102d2c0809faf215ae163
Massimiliano Montaleone <theplayergame97@gmail.com>
2018-05-21 10:48:30 +0200

    - Corretto l'invio della posizione

--- FindEAT_Bot/FindEAT_Bot.py
+++ FindEAT_Bot/FindEAT_Bot.py
@@ -1,11 +1,13 @@
-import os
-import sys
-import telepot
-import json
-import requests
 from settings import token, start_msg, client_file
+from telepot.loop import MessageLoop
 from telepot.namedtuple import ReplyKeyboardMarkup, ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton
 from time import sleep
+import json
+import os
+import requests
+import reverse_geocode
+import sys
+import telepot
 
 # State for user
 user_state = {}
@@ -36,7 +38,6 @@ def on_chat_message(msg):
         user_state[chat_id] = 2
 
     elif user_state[chat_id] == 2:
-
         if content_type == 'text':
             try:
                 r = requests.get(
@@ -58,43 +59,36 @@ def on_chat_message(msg):
                 user_state[chat_id] = 3
 
             except:
-                print("Error URL check README.md")
+                print("Errore API")
 
         if content_type == 'location':
+            # get location
+            coordinates = (str(msg['location']['latitude']),
+                           str(msg['location']['longitude'])),
+            a = reverse_geocode.search(coordinates)[0]['city']
 
             try:
-                # send location
-                response = requests.get('http://maps.googleapis.com/maps/api/geocode/json?latlng=' + str(
-                    msg['location']['latitude']) + ',' + str(msg['location']['longitude']))
-                resp_json_payload = response.json()
-                a = str(resp_json_payload['results'][0]
-                        ['address_components'][2]['long_name'])
+                r = requests.get(
+                    url=url_api + '?tipo=luogo&lista=' + a.lower())
+                json_data = r.json()
+                testo_salvato[chat_id] = a.lower()
 
-                try:
-                    r = requests.get(
-                        url=url_api + a.lower() + ".json")
-                    json_data = r.json()
-                    testo_salvato[chat_id] = a.lower()
+                array = ''
+                for i in range(5):
+                    nome = json_data['lista'][i]['nome']
+                    array += str(i + 1) + ': ' + nome + '\n'
 
-                    array = ''
-                    for i in range(5):
-                        nome = json_data['lista'][i]['nome']
-                        array += str(i + 1) + ': ' + nome + '\n'
+                bot.sendMessage(chat_id, array)
 
-                    bot.sendMessage(chat_id, array)
-
-                except:
-                    print("Error URL check README.md")
-
-                # bot.sendMessage(chat_id, 'Scrivi il nome/numero del ristorante')
-                msg = "Il ristorante è nella lista?"
-                markup = ReplyKeyboardMarkup(keyboard=[["Si", "No"]
-                                                       ])
-                bot.sendMessage(chat_id, msg, reply_markup=markup)
-                user_state[chat_id] = 3
             except:
-                bot.sendMessage(
-                    chat_id, "Errore chiave Google, rimanda la posizione...")
+                print("Errore API")
+
+            # bot.sendMessage(chat_id, 'Scrivi il nome/numero del ristorante')
+            msg = "Il ristorante è nella lista?"
+            markup = ReplyKeyboardMarkup(keyboard=[["Si", "No"]
+                                                   ])
+            bot.sendMessage(chat_id, msg, reply_markup=markup)
+            user_state[chat_id] = 3
 
     elif user_state[chat_id] == 3:
 
@@ -108,7 +102,6 @@ def on_chat_message(msg):
             user_state[chat_id] = 5
 
     elif user_state[chat_id] == 4:
-
         try:
             r = requests.get(
                 url=url_api + '?tipo=luogo&lista=' + testo_salvato[chat_id])
@@ -117,24 +110,33 @@ def on_chat_message(msg):
             if msg['text'].isdigit() and int(msg['text']) <= 5:
                 nome = json_data['lista'][int(msg['text']) - 1]['nome']
 
+                orari = json_data['lista'][int(
+                    msg['text']) - 1]['orari']
+                posizione = json_data['lista'][int(
+                    msg['text']) - 1]['posizione'].split(',')
                 apertura = json_data['lista'][int(
                     msg['text']) - 1]['apertura']
                 numtell = json_data['lista'][int(
                     msg['text']) - 1]['numtell']
                 valutazione = json_data['lista'][int(
                     msg['text']) - 1]['valutazione']
-                posizione = json_data['lista'][int(
-                    msg['text']) - 1]['posizione'].split(',')
 
-                bot.sendMessage(chat_id, '🍽 ' + nome + '\n🕐 ' + str(apertura).replace('None', 'Apertura non disponibile') + '\n📱 ' +
-                                str(numtell) + '\n⭐️ ' + str(valutazione))
+                if orari == None:
+                    orari = 'Orari non disponibili'
+                else:
+                    orari = '\n'.join(orari)
+
+                bot.sendMessage(chat_id, '🍽 ' + nome + '\n🕐 ' + str(apertura).replace('None', 'Apertura non disponibile') +
+                                '\n📱 ' + str(numtell) + '\n⭐️ ' + str(valutazione) + '\n------\n' + orari + '\nVedi i /feedback')
                 bot.sendLocation(chat_id, posizione[0], posizione[1])
+                user_state[chat_id] = 99
+
             else:
-                user_state[chat_id] == 4
+                user_state[chat_id] = 4
                 bot.sendMessage(chat_id, 'Scrivi il numero del ristorante')
 
         except:
-            print("Error URL check README.md")
+            print("Errore API")
 
     elif user_state[chat_id] == 5:
         try:
@@ -150,29 +152,42 @@ def on_chat_message(msg):
             posizione = json_data['lista'][0]['posizione'].split(',')
             orari = json_data['lista'][0]['orari']
 
-            if apertura == 'Aperto':
-                bot.sendMessage(chat_id, '🍽 ' + nome + '\n🕐 ' + str(apertura).replace('None', 'Apertura non disponibile') + '\n📱 ' +
-                                str(numtell) + '\n⭐️ ' + str(valutazione))
-            else:
-
-                if orari == None:
-                    orari = 'Orari non disponibili'
-                else:
-                    orari = '\n'.join(orari)
-
-                bot.sendMessage(chat_id, '🍽 ' + nome + '\n🕐 ' + str(apertura).replace('None', 'Apertura non disponibile') + '\n📱 ' +
-                                str(numtell) + '\n⭐️ ' + str(valutazione) + '\n------\n' + orari)
+            bot.sendMessage(chat_id, '🍽 ' + nome + '\n🕐 ' + str(apertura).replace('None', 'Apertura non disponibile') + '\n📱 ' +
+                            str(numtell) + '\n⭐️ ' + str(valutazione) + '\n------\n' + orari + '\nVedi i /feedback')
 
             bot.sendLocation(chat_id, posizione[0], posizione[1])
+            user_state[chat_id] = 99
 
         except:
             bot.sendMessage(chat_id, "Ristorante non trovato, riprova...")
             user_state[chat_id] = 5
 
+    elif user_state[chat_id] == 99:
+        if msg['text'] == '/feedback':
+            user_state[chat_id] = 1000
+
+            keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                [dict(text='indietro', callback_data='1'),
+                 dict(text='Avanti', callback_data='2')]
+            ])
+
+            bot.sendMessage(chat_id, 'Ecco i feedback di Google',
+                            reply_markup=keyboard)
+
+
+def on_callback_query(msg):
+    query_id, from_id, query_data = telepot.glance(
+        msg, flavor='callback_query')
+    print('Callback Query:', query_id, from_id, query_data)
+
+    if int(query_data) == 1:
+        bot.answerCallbackQuery(query_id, text='1')
+        bot.sendMessage(from_id, 'Work in progress')
+
 
 def register_user(chat_id):
     """
-    Register given user to receive news
+    Register user
     """
     insert = 1
 
@@ -213,7 +228,8 @@ f.write(pid)
 # Start working
 try:
     bot = telepot.Bot(token)
-    bot.message_loop(on_chat_message)
+    MessageLoop(bot, {'chat': on_chat_message,
+                      'callback_query': on_callback_query}).run_as_thread()
     while(1):
         sleep(10)
 finally:
