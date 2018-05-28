4a984ca049e0de4a0bd370dce28dddac9a7c0dc9
gg97g <giuseppettigiorgia97@gmail.com>
2018-05-28 18:53:47 +0200

    Aggiornato: README.md e il Bot

--- FindEAT_Bot/FindEAT_Bot.py
+++ FindEAT_Bot/FindEAT_Bot.py
@@ -128,27 +128,27 @@ def on_chat_message(msg):
 
     elif user_state[chat_id] == 8:
         if msg['text'] == 'Si':
-            filtri('/filters/'+place[chat_id] + '/'+restaurant[chat_id] + '/costi/alto',
-                   '/filters/'+place[chat_id] + '/'+restaurant[chat_id] + '/costi/', 'alto', {'alto': 0, 'basso': 0})
+            filtri('/filters/'+place[chat_id] + '/'+restaurant[chat_id] + '/costo/alto',
+                   '/filters/'+place[chat_id] + '/'+restaurant[chat_id] + '/costo/', 'alto', {'alto': 0, 'basso': 0})
 
         if msg['text'] == 'No':
 
-            filtri('/filters/'+place[chat_id] + '/'+restaurant[chat_id] + '/costi/basso',
-                   '/filters/'+place[chat_id] + '/'+restaurant[chat_id] + '/costi/', 'basso', {'alto': 0, 'basso': 0})
+            filtri('/filters/'+place[chat_id] + '/'+restaurant[chat_id] + '/costo/basso',
+                   '/filters/'+place[chat_id] + '/'+restaurant[chat_id] + '/costo/', 'basso', {'alto': 0, 'basso': 0})
 
         markup = ReplyKeyboardMarkup(keyboard=[["Si", "No"]])
         bot.sendMessage(
-            chat_id, 'Il ristorante prevede un menù SENZA GLUTINE?', reply_markup=markup)
+            chat_id, 'Il ristorante prevede un menù senza glutine?', reply_markup=markup)
         user_state[chat_id] = 9
 
     elif user_state[chat_id] == 9:
         if msg['text'] == 'Si':
-            filtri('/filters/'+place[chat_id] + '/'+restaurant[chat_id] + '/menu/0/celiaci/votosi',
-                   '/filters/'+place[chat_id] + '/'+restaurant[chat_id] + '/menu/0/celiaci', 'votosi', {'votosi': 0, 'votono': 0})
+            filtri('/filters/'+place[chat_id] + '/'+restaurant[chat_id] + '/menu/0/celiaci/votisi',
+                   '/filters/'+place[chat_id] + '/'+restaurant[chat_id] + '/menu/0/celiaci', 'votisi', {'votisi': 0, 'votino': 0})
 
         if msg['text'] == 'No':
-            filtri('/filters/'+place[chat_id] + '/'+restaurant[chat_id] + '/menu/0/celiaci/votono',
-                   '/filters/'+place[chat_id] + '/'+restaurant[chat_id] + '/menu/0/celiaci', 'votono', {'votosi': 0, 'votono': 0})
+            filtri('/filters/'+place[chat_id] + '/'+restaurant[chat_id] + '/menu/0/celiaci/votino',
+                   '/filters/'+place[chat_id] + '/'+restaurant[chat_id] + '/menu/0/celiaci', 'votino', {'votisi': 0, 'votino': 0})
 
         markup = ReplyKeyboardMarkup(keyboard=[["Si", "No"]])
         bot.sendMessage(
@@ -157,61 +157,96 @@ def on_chat_message(msg):
 
     elif user_state[chat_id] == 10:
         if msg['text'] == 'Si':
-            filtri('/filters/'+place[chat_id] + '/'+restaurant[chat_id] + '/menu/1/bambino/votosi',
-                   '/filters/'+place[chat_id] + '/'+restaurant[chat_id] + '/menu/1/bambino', 'votosi', {'votosi': 0, 'votono': 0})
+            filtri('/filters/'+place[chat_id] + '/'+restaurant[chat_id] + '/menu/1/bambino/votisi',
+                   '/filters/'+place[chat_id] + '/'+restaurant[chat_id] + '/menu/1/bambino', 'votisi', {'votisi': 0, 'votino': 0})
 
         if msg['text'] == 'No':
-            filtri('/filters/'+place[chat_id] + '/'+restaurant[chat_id] + '/menu/1/bambino/votono',
-                   '/filters/'+place[chat_id] + '/'+restaurant[chat_id] + '/menu/1/bambino', 'votono', {'votosi': 0, 'votono': 0})
+            filtri('/filters/'+place[chat_id] + '/'+restaurant[chat_id] + '/menu/1/bambino/votino',
+                   '/filters/'+place[chat_id] + '/'+restaurant[chat_id] + '/menu/1/bambino', 'votino', {'votisi': 0, 'votino': 0})
 
         bot.sendMessage(
-            chat_id, 'Grazie per aver risposto alle domande!!!🤞🏻🤝😄 ')
-        user_state[chat_id] = 0
+            chat_id, 'Grazie per aver risposto alle domande!!!🤞🏻🤝😄 ', reply_markup=ReplyKeyboardRemove(
+                remove_keyboard=True))
+        user_state[chat_id]=0
 
 
 def richiesta(url, msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
+    content_type, chat_type, chat_id=telepot.glance(msg)
 
     try:
-        r = requests.get(
-            url=url)
-        json_data = r.json()
-        url_salvato[chat_id] = url
+        r=requests.get(
+            url = url)
+        json_data=r.json()
+        url_salvato[chat_id]=url
 
-        nome = json_data['lista'][0]['nome']
-        orari = json_data['orari'][0]
-        posizione = json_data['lista'][0]['posizione'].split(',')
-        apertura = json_data['lista'][0]['apertura']
-        numtell = json_data['lista'][0]['numtell']
-        valutazione = json_data['lista'][0]['valutazione']
+        nome=json_data['lista'][0]['nome']
+        orari=json_data['orari'][0]
+        posizione=json_data['lista'][0]['posizione'].split(',')
+        apertura=json_data['lista'][0]['apertura']
+        numtell=json_data['lista'][0]['numtell']
+        valutazione=json_data['lista'][0]['valutazione']
 
         if orari == None:
-            orari = 'Orari non disponibili'
+            orari='Orari non disponibili'
         else:
-            orari = '\n'.join(orari)
+            orari='\n'.join(orari)
 
-        restaurant[chat_id] = nome
+        restaurant[chat_id]=nome
 
         filtri('/filters/'+place[chat_id] + '/'+restaurant[chat_id] + '/contatore/0',
                '/filters/'+place[chat_id] + '/'+restaurant[chat_id] + '/contatore/', '0', {'0': 0})
 
-        keyboard = InlineKeyboardMarkup(inline_keyboard=[
-            [dict(text='Visualizza feedback di Google', callback_data=1),
-             dict(text='Visualizza feedback di FindEAT', callback_data=7)]
+        keyboard=InlineKeyboardMarkup(inline_keyboard = [
+            [dict(text='Feedback di Google', callback_data=1),
+             dict(text='Feedback di FindEAT', callback_data=7)]
         ])
+
+        contatore=firebase.get('/filters/'+place[chat_id] + '/' +
+                                 restaurant[chat_id] + '/contatore/0', None)
+        cartadicredito=firebase.get(
+            '/filters/'+place[chat_id] + '/' + restaurant[chat_id] + '/cartadicredito/votisi', None)
+        cartadicredito1=firebase.get(
+            '/filters/'+place[chat_id] + '/' + restaurant[chat_id] + '/cartadicredito/votino', None)
+
+        costo=firebase.get(
+            '/filters/'+place[chat_id] + '/' + restaurant[chat_id] + '/costo/basso', None)
+        costo1=firebase.get(
+            '/filters/'+place[chat_id] + '/' + restaurant[chat_id] + '/costo/alto', None)
+
+        celiaci=firebase.get(
+            '/filters/'+place[chat_id] + '/' + restaurant[chat_id] + '/menu/0/celiaci/votisi', None)
+        celiaci1=firebase.get(
+            '/filters/'+place[chat_id] + '/' + restaurant[chat_id] + '/menu/0/celiaci/votino', None)
+
+        bambino=firebase.get(
+            '/filters/'+place[chat_id] + '/' + restaurant[chat_id] + '/menu/1/bambino/votisi', None)
+        bambino1=firebase.get(
+            '/filters/'+place[chat_id] + '/' + restaurant[chat_id] + '/menu/1/bambino/votino', None)
 
         if apertura == 'Aperto' or apertura == None:
             bot.sendMessage(chat_id, '🍽 ' + nome + '\n🕐 ' + str(apertura).replace('None', 'Apertura non disponibile') +
-                            '\n📱 ' + str(numtell) + '\n⭐️ ' + str(valutazione))
+                            '\n📱 ' + str(numtell) + '\n⭐️ ' + str(valutazione) + "\n\nFeedback by FindEAT\nCercato " +
+                            str(contatore).replace("None", "Non recensito")+" volte" + "\n💳 Carta di credito: Voti si: " + str(cartadicredito).replace("None", "Non recensito") + ", Voti no: " + str(cartadicredito1).replace("None", "Non recensito") +
+                            "\n💸 Costoso: Basso: " + str(costo) + ", Alto: " + str(costo1) +
+                            "\n📒 Menù glutenfree: Voti si: " + str(celiaci).replace("None", "Non recensito") + ", Voti no: " + str(celiaci1).replace("None", "Non recensito") +
+                            "\n👶🏼 Menù per bambini: Voti si: " +
+                            str(bambino).replace("None", "Non recensito") + ", Voti no: " + str(bambino1).replace("None", "Non recensito"))
 
         else:
             bot.sendMessage(chat_id, '🍽 ' + nome + '\n🕐 ' + str(apertura).replace('None', 'Apertura non disponibile') +
-                            '\n📱 ' + str(numtell) + '\n⭐️ ' + str(valutazione) + '\n------\n' + orari)
+                            '\n📱 ' + str(numtell) + '\n⭐️ ' + str(valutazione) + '\n------\n' + orari + "\n\nFeedback by FindEAT\nCercato " +
+                            str(contatore)+" volte" + "\n💳 Carta di credito: Voti si: " + str(cartadicredito).replace("None", "Non recensito") + ", Voti no: " + str(cartadicredito1).replace("None", "Non recensito") +
+                            "\n💸 Costoso: Basso: " + str(costo).replace("None", "Non recensito") + ", Alto: " + str(costo1).replace("None", "Non recensito") +
+                            "\n📒 Menù glutenfree: Voti si: " + str(celiaci).replace("None", "Non recensito") + ", Voti no: " + str(celiaci1).replace("None", "Non recensito") +
+                            "\n👶🏼 Menù per bambini: Voti si: " +
+                            str(bambino).replace("None", "Non recensito") + ", Voti no: " + str(bambino1).replace("None", "Non recensito")
+
+                            )
 
         bot.sendLocation(chat_id, posizione[0], posizione[1])
         bot.sendMessage(
-            chat_id, "Hai bisogno di aiuto?", reply_markup=keyboard)
-        user_state[chat_id] = 0
+            chat_id, "Hai bisogno di aiuto?", reply_markup = keyboard)
+        user_state[chat_id]=0
 
     except:
         print("Errore API")
@@ -220,136 +255,136 @@ def richiesta(url, msg):
 def filtri(get, patch, request, check):
 
     try:
-        result = firebase.get(get, None)
+        result=firebase.get(get, None)
 
-        n = int(result)
-        result = firebase.patch(patch, {request: n+1})
+        n=int(result)
+        result=firebase.patch(patch, {request: n+1})
 
     except:
-        result = firebase.patch(patch, check)
-        result = firebase.get(get, None)
+        result=firebase.patch(patch, check)
+        result=firebase.get(get, None)
 
-        n = int(result)
-        result = firebase.patch(patch, {request: n+1})
+        n=int(result)
+        result=firebase.patch(patch, {request: n+1})
 
 
 def cerca(luogo, msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    r = requests.get(
-        url=url_api + '?tipo=luogo&lista=' + luogo.lower())
-    json_data = r.json()
-    place[chat_id] = luogo.lower()
+    content_type, chat_type, chat_id=telepot.glance(msg)
+    r=requests.get(
+        url = url_api + '?tipo=luogo&lista=' + luogo.lower())
+    json_data=r.json()
+    place[chat_id]=luogo.lower()
 
-    array = ''
-    array1 = ''
+    array=''
+    array1=''
     for i in range(5):
-        nome = json_data['lista'][i]['nome']
-        placeid = json_data['lista'][i]['id']
+        nome=json_data['lista'][i]['nome']
+        placeid=json_data['lista'][i]['id']
         array += str(i + 1) + ': ' + nome + '\n'
         array1 += placeid + ','
 
-    place_id[chat_id] = array1.split(',')
+    place_id[chat_id]=array1.split(',')
 
-    markup = ReplyKeyboardMarkup(keyboard=[["Si", "No"]])
+    markup=ReplyKeyboardMarkup(keyboard = [["Si", "No"]])
     bot.sendMessage(
-        chat_id, array + "\nIl ristorante è nella lista?", reply_markup=markup)
+        chat_id, array + "\nIl ristorante è nella lista?", reply_markup = markup)
 
-    user_state[chat_id] = 3
+    user_state[chat_id]=3
 
 
 def on_callback_query(msg):
-    query_id, from_id, query_data = telepot.glance(
-        msg, flavor='callback_query')
-    edited = (from_id, msg['message']['message_id'])
+    query_id, from_id, query_data=telepot.glance(
+        msg, flavor = 'callback_query')
+    edited=(from_id, msg['message']['message_id'])
     try:
-        r = requests.get(url=url_salvato[from_id])
-        json_data = r.json()
+        r=requests.get(url = url_salvato[from_id])
+        json_data=r.json()
 
         if (query_data == str(1)):
 
-            lista = json_data['feedback'][int(query_data)-1]
-            autore = lista['author_name']
-            valurazione = str(lista['rating'])
-            commento = lista['text']
-            feedback = 'Autore: ' + autore + '\nValutazione: ' + \
+            lista=json_data['feedback'][int(query_data)-1]
+            autore=lista['author_name']
+            valurazione=str(lista['rating'])
+            commento=lista['text']
+            feedback='Autore: ' + autore + '\nValutazione: ' +\
                 valurazione + '\nFeedback: ' + commento
 
-            keyboard = InlineKeyboardMarkup(inline_keyboard=[
+            keyboard=InlineKeyboardMarkup(inline_keyboard = [
                 [dict(text='Avanti', callback_data=2)]
 
             ])
 
             bot.editMessageText(edited, feedback,
-                                reply_markup=keyboard)
+                                reply_markup = keyboard)
 
         if (query_data == str(2)):
-            keyboard = InlineKeyboardMarkup(inline_keyboard=[
+            keyboard=InlineKeyboardMarkup(inline_keyboard = [
                 [dict(text='Indietro', callback_data=1),
                  dict(text='Avanti', callback_data=3)]
             ])
 
-            lista = json_data['feedback'][int(query_data)-1]
-            autore = lista['author_name']
-            valurazione = str(lista['rating'])
-            commento = lista['text']
-            feedback = 'Autore: ' + autore + '\nValutazione: ' + \
+            lista=json_data['feedback'][int(query_data)-1]
+            autore=lista['author_name']
+            valurazione=str(lista['rating'])
+            commento=lista['text']
+            feedback='Autore: ' + autore + '\nValutazione: ' +\
                 valurazione + '\nFeedback: ' + commento
 
             bot.editMessageText(edited, feedback,
-                                reply_markup=keyboard)
+                                reply_markup = keyboard)
 
         if (query_data == str(3)):
-            keyboard = InlineKeyboardMarkup(inline_keyboard=[
+            keyboard=InlineKeyboardMarkup(inline_keyboard = [
                 [dict(text='Indietro', callback_data=2),
                  dict(text='Avanti', callback_data=4)]
             ])
 
-            lista = json_data['feedback'][int(query_data)-1]
-            autore = lista['author_name']
-            valurazione = str(lista['rating'])
-            commento = lista['text']
-            feedback = 'Autore: ' + autore + '\nValutazione: ' + \
+            lista=json_data['feedback'][int(query_data)-1]
+            autore=lista['author_name']
+            valurazione=str(lista['rating'])
+            commento=lista['text']
+            feedback='Autore: ' + autore + '\nValutazione: ' +\
                 valurazione + '\nFeedback: ' + commento
 
             bot.editMessageText(edited, feedback,
-                                reply_markup=keyboard)
+                                reply_markup = keyboard)
 
         if (query_data == str(4)):
-            keyboard = InlineKeyboardMarkup(inline_keyboard=[
+            keyboard=InlineKeyboardMarkup(inline_keyboard = [
                 [dict(text='Indietro', callback_data=3),
                  dict(text='Avanti', callback_data=5)]
             ])
 
-            lista = json_data['feedback'][int(query_data)-1]
-            autore = lista['author_name']
-            valurazione = str(lista['rating'])
-            commento = lista['text']
-            feedback = 'Autore: ' + autore + '\nValutazione: ' + \
+            lista=json_data['feedback'][int(query_data)-1]
+            autore=lista['author_name']
+            valurazione=str(lista['rating'])
+            commento=lista['text']
+            feedback='Autore: ' + autore + '\nValutazione: ' +\
                 valurazione + '\nFeedback: ' + commento
 
             bot.editMessageText(edited, feedback,
-                                reply_markup=keyboard)
+                                reply_markup = keyboard)
 
         if (query_data == str(5)):
-            keyboard = InlineKeyboardMarkup(inline_keyboard=[
+            keyboard=InlineKeyboardMarkup(inline_keyboard = [
                 [dict(text='Indietro', callback_data=4),
                  dict(text='Fine', callback_data=6)]
             ])
 
-            lista = json_data['feedback'][int(query_data)-1]
-            autore = lista['author_name']
-            valurazione = str(lista['rating'])
-            commento = lista['text']
-            feedback = 'Autore: ' + autore + '\nValutazione: ' + \
+            lista=json_data['feedback'][int(query_data)-1]
+            autore=lista['author_name']
+            valurazione=str(lista['rating'])
+            commento=lista['text']
+            feedback='Autore: ' + autore + '\nValutazione: ' +\
                 valurazione + '\nFeedback: ' + commento
 
             bot.editMessageText(edited, feedback,
-                                reply_markup=keyboard)
+                                reply_markup = keyboard)
 
         if (query_data == str(6)):
             bot.editMessageText(edited, 'Fine')
 
-            keyboard = InlineKeyboardMarkup(inline_keyboard=[
+            keyboard=InlineKeyboardMarkup(inline_keyboard = [
                 [dict(text='Visualizza feedback di Google', callback_data=1)]
             ])
             bot.editMessageText(
@@ -388,7 +423,7 @@ def on_callback_query(msg):
             o = len(result)
             feedback = ''
             for i in range(int(o)):
-                feedback = feedback + \
+                feedback = feedback +\
                     str(i+1)+') Autore: '+result[i]['author'] + \
                     ' \nFeedback: ' + result[i]['text'] + '\n\n'
 
--- README.md
+++ README.md
@@ -27,3 +27,28 @@ Il progetto è composto da 3 obbiettivi principali:
  * Realizzazione di un API (GET) in NodeJS
  * Implementazione di un BotTelegram (Python)
  * Implementazione di un'app Android (JAVA)
+
+
+
+<div align="center"><a><img src='img/schema.png' height='400' alt='icon'/></a></div>
+
+
+ ----------------------------------------------------
+## FindEAT_Bot ##
+ 
+FindEAT_Bot è il bot pensato per gli utenti che possono comodamente ricercare il loro ristorante preferito. Il bot permette di ricercare per zona o per posizione. Esso ristituisce all’utente i primi 5 ristoranti che riesce a trovare, se il ristorante desiderato non è nella lista è possibile fare una ricerca diretta scrivendo il nome del locale. Una volta trovato il locale, verranno inviate delle informazioni, come: nome, apertura/chiusura, numero di telefono, valutazione, eventuali orari nel caso il ristorante fosse chiuso e la posizione. Inoltre vengono visualizzati dei feedback utente come: l’uso del pos, se il costo è mediamente alto o basso e  la possibilità di un menù senza glutine o per bambini. Infine i feedback scritti dagli utenti di Google e quelli di FindEAT. 
+
+Questo bot è stato realizzato in Python e si interfaccia alle API di Google ed al database di Firebase.
+
+Abbiamo un solo semplice comando:
+
+```
+./cerca: permette di cercare il ristorante per zona o posizione    
+```
+ 
+ Ovviamente il bot è una versione ‘lite’, schematizzata e molto semplice da utilizzare e gestire.
+
+
+
+
+
